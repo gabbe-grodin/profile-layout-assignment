baf3f151c97fdecf5ab6f5d7a2f5ea19165202fc
Gabbe Grodin, MFA <gabbe.grodin@gmail.com>
2023-06-21 15:45:17 -0500

    began adding JS, fixed layout misalignment, began converting some divs to uls

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("connected to html")
+
+var mainUser = document.querySelector("#main-user");
+
+function editName(element) {
+        // console.log("function seen");
+        if(mainUser.innerText = "Jane Doe") {
+                mainUser.innerText = "Commander Benjamin Lafayette Sisko";
+        } else {
+                mainUser.innerText = "Jane Doe";
+        }
+        
+}
