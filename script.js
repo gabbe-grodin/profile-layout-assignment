586821be13adcd38082d77ca781d1d228aea2222
Gabbe Grodin, MFA <gabbe.grodin@gmail.com>
2023-06-24 08:53:36 -0500

    added js and made html and css more dry

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
+// with querySelectors:
+
+// var mainUser = document.querySelector("main-user");
+
+// function editName() {
+//         console.log("function seen");
+//         mainUser.innerText = "Commander Benjamin Lafayette Sisko";
+
+//         // if(mainUser.innerText === "Jane Doe") {
+//         //         mainUser.innerText = "Commander Benjamin Lafayette Sisko";
+//         // } else {
+//         //         mainUser.innerText = "Jane Doe"; // why doesn't this return mainUser to Jane?
+//         // }        
+// }
+
+// let element = document.querySelector('#1');
+
+// function removeRequest(id) {
+//         let element = document.querySelector(id);
+//         element.remove();
+//         // add this to removeRequest function also: decrement #request-count
+        
+// }
+
+
+//////////////////////////////////
+
+// with getElementByID:
+
 // console.log("connected to html")
 
-var mainUser = document.querySelector("#main-user");
+var mainUser = document.getElementById("main-user");
 
-function editName(element) {
-        // console.log("function seen");
-        if(mainUser.innerText = "Jane Doe") {
-                mainUser.innerText = "Commander Benjamin Lafayette Sisko";
-        } else {
-                mainUser.innerText = "Jane Doe";
+function editName() {
+        console.log("function seen");
+        mainUser.innerText = "Commander Benjamin Lafayette Sisko";
+        // how to change back to Jane?
+}
+
+
+function removeRequest(id) {
+        let element = document.getElementById(id);
+        element.remove();
+        
+        function decrementRequestCount() {
+                let requestCount = document.getElementById("request-count").innerText;requestCount = requestCount--;
+        }
+        if () {
+                // increment innerText of h3 with id of 'connection-count'
         }
         
 }
+
+// clicking accept button increments #connection-count. must this be a new func?
